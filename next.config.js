ecf2b2fcf18277747078da94047c23205c86bc6d
Marko Markovic <archorrs@gmail.com>
2022-06-23 15:28:56 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -29,11 +29,7 @@ module.exports = {
   images: {
     domains: [
       'www.datocms-assets.com',
-      'a.storyblok.com',
-      'images.ctfassets.net',
-      'images.prismic.io',
-      'cdn.aglty.io',
-      'localhost' // For Strapi
+      
     ],
     imageSizes: [24, 64, 300]
   }
